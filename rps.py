149b25a7b5d87b82e264c4f273299067e81921cd
unknown <ayseturkk01@hotmail.com>
2025-02-06 21:41:46 +0300

    loops added

--- rps.py
+++ rps.py
@@ -15,31 +15,44 @@ class RPS(Enum):
 #print (RPS.ROCK.value)
 #sys.exit()
 
-print("")
-playerchoice =input("Enter...\n1 for rock\n2 for paper, or \n3 for Scissors:\n\n")
+playagain = True
 
-player = int(playerchoice)
+while playagain: 
 
-if player <1 or player >3:
-    sys.exit("you must enter 1, 2, or 3.")
+   
+    playerchoice =input("\nEnter...\n1 for rock\n2 for paper, or \n3 for Scissors:\n\n")
 
-computerchoice = random.choice("123")
-computer =int(computerchoice)
+    player = int(playerchoice)
 
-print("")
+    if player <1 or player >3:
+        sys.exit("you must enter 1, 2, or 3.")
 
-print ("you choose "+ str(RPS(player)).replace("RPS.", '') + ".")
-print ("python choose "+ str(RPS(computer)).replace("RPS.", '') + ".")
+    computerchoice = random.choice("123")
+    computer =int(computerchoice)
 
-print("")
+    
 
-if player == 1 and computer == 3:
-    print ("you win!")
-elif player == 2 and computer == 1:
-    print ("you win!")
-elif player == 3 and computer == 2:
-    print ("you win!")
-elif player == computer:
-    print("Tie game!")
-else:
-    print("python win!")
+    print ("\nyou choose "+ str(RPS(player)).replace("RPS.", '') + ".")
+    print ("python choose "+ str(RPS(computer)).replace("RPS.", '') + ".\n")
+
+
+    if player == 1 and computer == 3:
+        print ("you win!")
+    elif player == 2 and computer == 1:
+        print ("you win!")
+    elif player == 3 and computer == 2:
+        print ("you win!")
+    elif player == computer:
+        print("Tie game!")
+    else:
+        print("python win!")
+    
+    playagain = input("\nPlay Again? \nY for yes or \nQ to Quit. \n\n")
+    
+    if playagain.lower() == "y":
+        continue
+    else:
+        print ("\nThank you for playing!\n")
+        playagain =False
+
+sys.exit("Bye!")
